6575cc21d04b18f9f0971374575f038a236eded4
Nicolae Cindea <nicolae.cindea@uca.fr>
2021-03-01 15:15:51 +0100

    Modify some functions for 1d assembling.

--- src/assemble1d.jl
+++ src/assemble1d.jl
@@ -20,21 +20,23 @@ Assemble a finite elements matrix corresponding to a 1 dimensional uniform mesh.
 # Arguments
   * `elem` : elementary finite elements matrix
   * `nbNodes` : number of nodes in the mesh
-  * `intNodes` : number of interior nodes in the interior of each elements
+  * `intNodes1` : number of interior nodes in the interior of each element for lhs
+  * `intNodes2` : number of interior nodes in the interior of each element for rhs
   * `dof1`     : number of degrees of freedom for each node for lhs
   * `dof2`     : number of degrees of freedom for each node for rhs
 """
 function assemble_1d_FE_matrix(elem::Array{Float64, 2}, nbNodes::Int64;
-                               intNodes = 0, dof1 = 1, dof2 = 1)
+                               intNodes1 = 0, intNodes2 = 0, dof1 = 1, dof2 = 1)
 
-    nbNodesTotal = (nbNodes + (nbNodes - 1) * intNodes)
+    nbNodesTotal1 = (nbNodes + (nbNodes - 1) * intNodes1)
+    nbNodesTotal2 = (nbNodes + (nbNodes - 1) * intNodes2)
 
-    M = spzeros(Float64, nbNodesTotal * dof1, nbNodesTotal * dof2)
+    M = spzeros(Float64, nbNodesTotal1 * dof1, nbNodesTotal2 * dof2)
     for i = 1:nbNodes - 1
-        l1 = (i - 1 + (i - 1) * intNodes) * dof1 + 1
-        r1 = (i + 1 + i * intNodes) * dof1
-        l2 = (i - 1 + (i - 1) * intNodes) * dof2 + 1
-        r2 = (i + 1 + i * intNodes) * dof2
+        l1 = (i - 1 + (i - 1) * intNodes1) * dof1 + 1
+        r1 = (i + 1 + i * intNodes1) * dof1
+        l2 = (i - 1 + (i - 1) * intNodes2) * dof2 + 1
+        r2 = (i + 1 + i * intNodes2) * dof2
         M[l1:r1, l2:r2] = M[l1:r1, l2:r2] + elem
     end
     M
@@ -49,22 +51,24 @@ Assemble a finite elements matrix corresponding to a 1 dimensional non-uniform m
 # Arguments
   * `elem` : elementary finite elements matrix (with elements of type SymPy.Sym)
   * `nodes` : vector of nodes composing the non uniform-mesh
-  * `intNodes` : number of interior nodes in the interior of each elements
+  * `intNodes1` : number of interior nodes in the interior of each element for lhs
+  * `intNodes2` : number of interior nodes in the interior of each element for rhs
   * `dof1`     : number of degrees of freedom for each node for lhs
   * `dof2`     : number of degrees of freedom for each node for rhs
 """
 function assemble_1d_nu_FE_matrix(elem::Matrix{SymPy.Sym}, nodes::Array{Float64, 1};
-                                  intNodes = 0, dof1 = 1, dof2 = 1)
+                                  intNodes1 = 0, intNodes2 = 0, dof1 = 1, dof2 = 1)
     global h  
     nbNodes = length(nodes)
-    nbNodesTotal = (nbNodes + (nbNodes - 1) * intNodes)
+    nbNodesTotal1 = (nbNodes + (nbNodes - 1) * intNodes1)
+    nbNodesTotal2 = (nbNodes + (nbNodes - 1) * intNodes2)
 
-    M = spzeros(Float64, nbNodesTotal * dof1, nbNodesTotal * dof2)
+    M = spzeros(Float64, nbNodesTotal1 * dof1, nbNodesTotal2 * dof2)
     for i = 1:nbNodes - 1
-        l1 = (i - 1 + (i - 1) * intNodes) * dof1 + 1
-        r1 = (i + 1 + i * intNodes) * dof1
-        l2 = (i - 1 + (i - 1) * intNodes) * dof2 + 1
-        r2 = (i + 1 + i * intNodes) * dof2
+        l1 = (i - 1 + (i - 1) * intNodes1) * dof1 + 1
+        r1 = (i + 1 + i * intNodes1) * dof1
+        l2 = (i - 1 + (i - 1) * intNodes2) * dof2 + 1
+        r2 = (i + 1 + i * intNodes2) * dof2
         elem_loc = elem.subs(h, nodes[i + 1] - nodes[i])
         M[l1:r1, l2:r2] = M[l1:r1, l2:r2] + elem_loc
     end
