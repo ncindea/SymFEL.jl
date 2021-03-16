0f7538d683bc6b3a5241f87140c8862cbc04aee8
Nicolae Cindea <nicolae.cindea@uca.fr>
2021-03-16 09:52:13 +0100

    Add support for multi-threading in assemble3d.

--- src/assemble3d.jl
+++ src/assemble3d.jl
@@ -22,7 +22,9 @@ function assemble_cubemesh_FE_matrix(el_mat::Array{Float64, 2},
                                      order2 = 1,
                                      dof1 = 1,
                                      dof2 = 1)
-    
+
+    nT = Threads.nthreads()
+
     n_order1 = (order1+1)*(4 + (order1 - 1) * (order1 + 3))
     n_order2 = (order2+1)*(4 + (order2 - 1) * (order2 + 3))
 
@@ -45,28 +47,34 @@ function assemble_cubemesh_FE_matrix(el_mat::Array{Float64, 2},
    
     nodes1_N = length(nodes1)
     nodes2_N = length(nodes2)
-    
-    nodes1_i = zeros(Int64, maximum(nodes1))
-    nodes1_i[nodes1] = 1:nodes1_N
-    nodes2_i = zeros(Int64, maximum(nodes2))
-    nodes2_i[nodes2] = 1:nodes2_N
 
     elements_N = size(elements, 2)
-    M = spzeros(Float64, nodes1_N * dof1, nodes2_N * dof2)
-    l2 = zeros(Int64, n_order1 * dof1)
-    r2 = zeros(Int64, n_order2 * dof2)
-    for i = 1:elements_N
-        l = el1[1:n_order1, i]
-        r = el2[1:n_order2, i]
-        l = nodes1_i[l]
-        r = nodes2_i[r]
+    MM = Array{SparseMatrixCSC{Float64,Int64}}(undef, nT)
+    ll2 = 
+    for i = 1:nT
+        MM[i] = spzeros(Float64, nodes1_N * dof1, nodes2_N * dof2)
+    end
+    
+    l2 = zeros(Int64, n_order1 * dof1, nT)
+    r2 = zeros(Int64, n_order2 * dof2, nT)
+
+    v1 = @. dof1 * ((1:n_order1)-1)
+    v2 = @. dof2 * ((1:n_order2)-1)
+    
+    Threads.@threads for i = 1:elements_N
+        k = Threads.threadid()
         for j = 1:dof1
-            l2[dof1 * ((1:n_order1).-1) .+ j] = dof1 * (l .- 1)  .+ j
+            l2[v1  .+ j, k] = @. dof1 * (el1[1:n_order1, i] - 1)  + j
         end
         for j = 1:dof2
-            r2[dof2 * ((1:n_order2).-1) .+ j] = dof2 * (r .- 1)  .+ j
+            r2[v2  .+ j, k] = @. dof2 * (el2[1:n_order2, i] - 1)  + j
         end
-        M[l2, r2] +=  el_mat
+        
+        MM[k][l2[:,k], r2[:,k]] += el_mat
+    end
+    M = MM[1]
+    for i=2:nT
+        M += MM[i]
     end
     M
 end
@@ -99,6 +107,8 @@ function assemble_cubemesh_FE_matrix(el_mat::Array{Float64, 2},
                                      order2 = 1,
                                      dof1 = 1,
                                      dof2 = 1)
+
+    nT = Threads.nthreads()
     
     n_order1 = (order1+1)*(4 + (order1 - 1) * (order1 + 3))
     n_order2 = (order2+1)*(4 + (order2 - 1) * (order2 + 3))
@@ -122,29 +132,37 @@ function assemble_cubemesh_FE_matrix(el_mat::Array{Float64, 2},
     
     nodes1_N = length(nodes1)
     nodes2_N = length(nodes2)
-    
-    nodes1_i = zeros(Int64, maximum(nodes1))
-    nodes1_i[nodes1] = 1:nodes1_N
-    nodes2_i = zeros(Int64, maximum(nodes2))
-    nodes2_i[nodes2] = 1:nodes2_N
 
     elements_N = size(elements, 2)
     labels_N = length(el_labels)
-    M = spzeros(Float64, nodes1_N * dof1, nodes2_N * dof2)
-    l2 = zeros(Int64, n_order1 * dof1)
-    r2 = zeros(Int64, n_order2 * dof2)
-    for i = 1:labels_N
-        l = el1[1:n_order1, el_labels[i]]
-        r = el2[1:n_order2, el_labels[i]]
-        l = nodes1_i[l]
-        r = nodes2_i[r]
+
+    MM = Array{SparseMatrixCSC{Float64,Int64}}(undef, nT)
+    ll2 = 
+    for i = 1:nT
+        MM[i] = spzeros(Float64, nodes1_N * dof1, nodes2_N * dof2)
+    end
+    
+    l2 = zeros(Int64, n_order1 * dof1, nT)
+    r2 = zeros(Int64, n_order2 * dof2, nT)
+
+    v1 = @. dof1 * ((1:n_order1)-1)
+    v2 = @. dof2 * ((1:n_order2)-1)
+
+    
+    Threads.@threads for i = 1:labels_N
+        k = Threads.threadid()
+
         for j = 1:dof1
-            l2[dof1 * ((1:n_order1).-1) .+ j] = dof1 * (l .- 1)  .+ j
+            l2[v1 .+ j, k] = @. dof1 * (el1[1:n_order1, el_labels[i]] - 1)  + j
         end
         for j = 1:dof2
-            r2[dof2 * ((1:n_order2).-1) .+ j] = dof2 * (r .- 1)  .+ j
+            r2[v2 .+ j, k] = @. dof2 * (el2[1:n_order2, el_labels[i]] - 1)  + j
         end
-        M[l2, r2] +=  el_mat
+        MM[k][l2[:, k], r2[:, k]] +=  el_mat
+    end
+    M = MM[1]
+    for i=2:nT
+        M += MM[i]
     end
     M
 end
@@ -177,6 +195,8 @@ function assemble2d_cubemesh_FE_matrix(el_mat::Array{Float64, 2},
                                        dof1 = 1,
                                        dof2 = 1)
     
+    nT = Threads.nthreads()
+
     n_order1 = (order1+1)*(4 + (order1 - 1) * (order1 + 3))
     n_order2 = 4 + (order1 - 1) * (order1 + 3)
 
@@ -186,22 +206,31 @@ function assemble2d_cubemesh_FE_matrix(el_mat::Array{Float64, 2},
     
 
     elements2d_N = size(elements2d, 2)
-    M = spzeros(Float64, nodes_N * dof1, nodes_N * dof2)
-
-    r2 = zeros(Int64, n_order2 * dof2)
-    l2 = zeros(Int64, n_order2 * dof2)
-    for i = 1:elements2d_N
-        l = elements2d[1:n_order2, i]
-        r = elements2d[1:n_order2, i]
-        
+    MM = Array{SparseMatrixCSC{Float64,Int64}}(undef, nT)
+    ll2 = 
+    for i = 1:nT
+        MM[i] = spzeros(Float64, nodes_N * dof1, nodes_N * dof2)
+    end
+    
+    l2 = zeros(Int64, n_order2 * dof2, nT)
+    r2 = zeros(Int64, n_order2 * dof2, nT)
+    v1 = dof2 * ((1:n_order2).-1)
+    v2 = dof2 * ((1:n_order2).-1)
+    
+    Threads.@threads for i = 1:elements2d_N
+        k = Threads.threadid()
         
         for j = 1:dof2
-            l2[dof2 * ((1:n_order2).-1) .+ j] = dof1 * (l .- 1)  .+ j
+            l2[v1 .+ j, k] = @. dof1 * (elements2d[1:n_order2, i] - 1)  + j
         end
         for j = 1:dof2
-            r2[dof2 * ((1:n_order2).-1) .+ j] = dof2 * (r .- 1)  .+ j
+            r2[v2 .+ j, k] = @. dof2 * (elements2d[1:n_order2, i] - 1)  + j
         end
-        M[l2, r2] +=  el_mat
+        MM[k][l2[:, k], r2[:, k]] +=  el_mat
+    end
+    M = MM[1]
+    for i=2:nT
+        M += MM[i]
     end
     M
 end
