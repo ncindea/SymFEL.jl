94c5840516a1dcd51edf6a49a74881f802db1868
Nicolae Cindea <nicolae.cindea@uca.fr>
2025-05-20 10:44:56 +0200

    Add suport for variable multiplicative coefficients in 1d
    The ceofficients are represented on some FEM space.

--- src/SymFEL.jl
+++ src/SymFEL.jl
@@ -152,6 +152,78 @@ function get_em(deg1=1, deg2=1, der1=0, der2=0; fe1="Lagrange", fe2="Lagrange")
     M
 end
 
+"""
+    get_etensor(deg1=1, deg2=1, deg=1, der1=0, der2=0, der3=0; fe1="Lagrange", fe2="Lagrange", fe3="Lagrange")
+
+Gives 1d elementary tensors for Lagrange and Hermite elements.
+
+This is used, in particular, for $∫ a(x) u(x) ϕ(x) dx$.
+
+The elementary matrices are computed for elements of length `h`.
+"""
+function get_etensor(deg1=1, deg2=1, deg3=1, der1=0, der2=0, der3=0; fe1="Lagrange", fe2="Lagrange", fe3="Lagrange")
+    global x
+    global h
+    if fe1 == "Lagrange"
+        if der1 ∈ collect(0:deg1)
+            p1 = get_lagrange_basis(deg1)
+        else
+            error("The third argument should verify der1 ∈ [0, …, deg1]...")
+        end
+    end
+    if fe2 == "Lagrange"
+        if der2 ∈ collect(0:deg2)
+            p2 = get_lagrange_basis(deg2)
+        else
+            error("The fourth parameter should verify der2 ∈ [0, …, deg2]...")
+        end
+    end
+    if fe3 == "Lagrange"
+        if der3 ∈ collect(0:deg3)
+            p3 = get_lagrange_basis(deg3)
+        else
+            error("The fourth parameter should verify der3 ∈ [0, …, deg3]...")
+        end
+    end
+    if fe1 == "Hermite"
+        if der1 ∈ collect(0:floor(Int, deg1 / 2 + 1))
+            p1 = get_hermite_basis(deg1)
+        else
+            error("The third parameter should verify der1 ∈ [0, …,int(deg1 / 2 + 1)]...")
+        end
+    end
+    if fe2 == "Hermite"
+        if der2 ∈ collect(0:floor(Int, deg2 / 2 + 1))
+            p2 = get_hermite_basis(deg2)
+        else
+            error("The fourth parameter should verify der2 ∈ [0, …,int(deg2 / 2 + 1)]...")
+        end
+    end
+    if fe3 == "Hermite"
+        if der3 ∈ collect(0:floor(Int, deg3 / 2 + 1))
+            p3 = get_hermite_basis(deg3)
+        else
+            error("The fourth parameter should verify der3 ∈ [0, …,int(deg3 / 2 + 1)]...")
+        end
+    end
+    if (fe1 != "Lagrange" && fe1 != "Hermite") || (fe2 != "Lagrange" && fe2 != "Hermite") || (fe3 != "Lagrange" && fe3 != "Hermite")
+        error("Only Lagrange and Hermite FEM are implemented.")
+    end
+
+    l1 = length(p1)
+    l2 = length(p2)
+    l3 = length(p3)
+    M = Array{SymPy.Sym}(undef, l1, l2, l3)
+    for i = 1:l1
+        for j = 1:l2
+            for k = 1:l3
+                M[i, j, k] = simplify(integrate(diff(p1[i], x, der1) * diff(p2[j], x, der2) * diff(p3[k], x, der3), (x, 0, h)))
+            end
+        end
+    end
+    M
+end
+
 
 """
     get_square_em(Mx::Array{SymPy.Sym, 2},
--- src/assemble1d.jl
+++ src/assemble1d.jl
@@ -61,6 +61,90 @@ function assemble_1d_FE_matrix(elem::Array{Float64, 2}, nbNodes::Int64;
 end
 
 """
+    assemble_1d_FE_matrix_multcoeff(elet::Array{Float64, 3},
+                                    coeff::Vector{Float64},
+                                    nbNodes::Int64;
+                                    intNodes1 = 0,
+                                    intNodes2 = 0,
+                                    intNodes3 = 0,
+                                    dof1 = 1,
+                                    dof2 = 1,
+                                    dof3 = 1)
+Assemble a finite elements matrix corresponding to a 1 dimensional uniform mesh with a variable coefficient.
+
+# Arguments
+  * `elet` : elementary finite elements tensor
+  * `coeff` : multiplicative coefficient (corresponding to 3rd FEM)
+  * `nbNodes` : number of nodes in the mesh
+  * `intNodes1` : number of interior nodes in the interior of each element for lhs
+  * `intNodes2` : number of interior nodes in the interior of each element for rhs
+  * `intNodes4` : number of interior nodes in the interior of each element for phs
+  * `dof1`     : number of degrees of freedom for each node for lhs
+  * `dof2`     : number of degrees of freedom for each node for rhs
+  * `dof3`     : number of degrees of freedom for each node for phs
+"""
+function assemble_1d_FE_matrix_multcoeff(elet::Array{Float64, 3},
+                                         coeff::Vector{Float64},
+                                         nbNodes::Int64;
+                                         intNodes1 = 0,
+                                         intNodes2 = 0,
+                                         intNodes3 = 0,
+                                         dof1 = 1,
+                                         dof2 = 1,
+                                         dof3 = 1)
+
+    nT = Threads.nthreads()
+    
+    nbNodesTotal1 = (nbNodes + (nbNodes - 1) * intNodes1)
+    nbNodesTotal2 = (nbNodes + (nbNodes - 1) * intNodes2)
+
+    MM = Array{SparseMatrixCSC{Float64,Int64}}(undef, nT)
+    for i = 1:nT
+        MM[i] = spzeros(Float64, nbNodesTotal1 * dof1, nbNodesTotal2 * dof2)
+    end
+
+    ls = size(elet, 1)
+    rs = size(elet, 2)
+    ps = size(elet, 3)
+    l1 = zeros(UInt64, nT)
+    r1 = zeros(UInt64, nT)
+    l2 = zeros(UInt64, nT)
+    r2 = zeros(UInt64, nT)
+    l3 = zeros(UInt64, nT)
+    r3 = zeros(UInt64, nT)
+
+    elem = Array{Array{SymPy.Sym}(undef, size(elet, 1), size(elet, 2)), nT)
+    
+
+    Threads.@threads for i = 1:(nbNodes - 1)
+        k = Threads.threadid()
+
+        for il = 1:ls
+            for ir = 1:rs
+                elem[k][il, ir] = 0
+            end
+        end
+        
+        l1[k] = (i - 1 + (i - 1) * intNodes1) * dof1 + 1
+        r1[k] = (i + 1 + i * intNodes1) * dof1
+        l2[k] = (i - 1 + (i - 1) * intNodes2) * dof2 + 1
+        r2[k] = (i + 1 + i * intNodes2) * dof2
+        l3[k] = (i - 1 + (i - 1) * intNodes3) * dof3 + 1
+        r3[k] = (i + 1 + i * intNodes3) * dof3
+        for ip = l3[k]:r3[k]
+            elem[k] += coeff[ip] * elet[:,:,ip - l3[k] + 1]
+        end
+        
+        MM[k][l1[k]:r1[k], l2[k]:r2[k]] += elem[k]
+    end
+    M = MM[1]
+    for i=2:nT
+        M += MM[i]
+    end
+    M
+end
+
+"""
     assemble_1d_FE_symmatrix(elem::Array{Sym, 2}, nbNodes::Int64;
       intNodes = 0, dof1 = 1, dof2 = 1)
 
